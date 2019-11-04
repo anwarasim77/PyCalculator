d54e56585aaba6eebde8908072a5f62fed9a37bd
Asim <anwarasim77@gmail.com>
2019-11-04 01:12:06 -0500

    Task: added absolutepath.py

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from Calculator import Calculator
+from CsvReader import CsvReader
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
@@ -10,22 +11,26 @@ class MyTestCase(unittest.TestCase):
         self.assertIsInstance(self.calculator, Calculator)
 
     def test_subtraction(self):
-            self.assertEqual(self.calculator.subtract(1,2), 1)
+        test_data = CsvReader("Data Files/Unit Test Subtraction.csv").data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.subtract(float(row['Value 1']), float(row['Value 2']), result))
+            self.assertEqual(self.calculator.result, result)
 
     def test_addition(self):
-            self.assertEqual(self.calculator.add(1,2), 3)
+        self.assertEqual(self.calculator.add(1,2), 3)
 
     def test_multiplication(self):
-            self.assertEqual(self.calculator.multiply(1,2), 2)
+        self.assertEqual(self.calculator.multiply(1,2), 2)
 
     def test_division(self):
-            self.assertEqual(self.calculator.divide(1,2), 2)
+        self.assertEqual(self.calculator.divide(1,2), 2)
 
     def test_square(self):
-            self.assertEqual(self.calculator.sqr(4), 16)
+        self.assertEqual(self.calculator.sqr(4), 16)
 
     def test_squareroot(self):
-            self.assertEqual(self.calculator.sqrt(16), 4)
+        self.assertEqual(self.calculator.sqrt(16), 4)
 
 if __name__ == '__main__':
     unittest.main()
